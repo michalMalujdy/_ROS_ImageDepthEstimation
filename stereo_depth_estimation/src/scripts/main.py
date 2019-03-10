5543e6314a49beba1c4a5d1a99d07aa037743eb3
Dexter <natri4x@gmail.com>
2019-03-10 21:00:26 +0100

    Run estimation once both images are present

--- stereo_depth_estimation/src/scripts/main.py
+++ stereo_depth_estimation/src/scripts/main.py
@@ -2,11 +2,47 @@
 
 import rospy
 from sensor_msgs.msg import Image
-from std_msgs.msg import String
+#from network import estimate_depth
 
 
-def img_subscriber(value):
-    rospy.loginfo('Received img {}'.format(value))
+left_img = None
+left_img_present = False
+
+right_img = None
+right_img_present = False
+
+def run_network_if_both_images_are_present():
+    global left_img_present, right_img_present
+    if left_img_present and right_img_present:
+        rospy.loginfo('Both side images are received, starting depth estimation...')
+        #depth = estimate_depth(left_img, right_img)
+        depth = 1 # to remove once tensorflow and network is set
+        rospy.loginfo('Depth estimation finished!')
+        rospy.loginfo('Depth: {}'.format(depth))
+
+        left_img_present = False
+        right_img_present = False
+
+
+def left_img_subscriber(img):
+    global left_img, left_img_present
+    rospy.loginfo('Received left image')
+
+    if left_img_present == False:
+        left_img = img
+        left_img_present = True
+        run_network_if_both_images_are_present()
+
+
+
+def right_img_subscriber(img):
+    global right_img, right_img_present
+    rospy.loginfo('Received right image')
+
+    if right_img_present == False:
+        right_img = img
+        right_img_present = True
+        run_network_if_both_images_are_present()
 
 
 def string_sub(value):
@@ -15,9 +51,8 @@ def string_sub(value):
 
 def main():
     rospy.init_node('stereo_depth_estimation', anonymous = True)
-    rospy.Subscriber("stereo_depth_estimation/img/left", Image, img_subscriber)
-    rospy.Subscriber("stereo_depth_estimation/img/right", Image, img_subscriber)
-    rospy.Subscriber("chatter", String, string_sub)
+    rospy.Subscriber("stereo_depth_estimation/img/left", Image, left_img_subscriber)
+    rospy.Subscriber("stereo_depth_estimation/img/right", Image, right_img_subscriber)
     rospy.spin()
 
 
