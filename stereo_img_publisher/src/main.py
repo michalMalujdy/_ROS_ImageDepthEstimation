c383056d7c6a3f13606a5ca3985211aa9fa03d9a
Dexter <natri4x@gmail.com>
2019-03-10 19:40:43 +0100

    Move functionality to a class

--- stereo_img_publisher/src/main.py
+++ stereo_img_publisher/src/main.py
@@ -6,28 +6,28 @@ from cv_bridge import CvBridge
 import cv2 as cv
 from std_msgs.msg import String
 
-left_img_path = '/media/dexter/Xubuntu/catkin_ws/src/stereo_img_publisher/img/left/000199_10.png'
-right_img_path = '/media/dexter/Xubuntu/catkin_ws/src/stereo_img_publisher/img/right/000199_10.png'
+class StereoImagePublisherNode:
+    def __init__(self):
+        self.cv_bridge = CvBridge()
+        self.left_img_path = '/media/dexter/Xubuntu/catkin_ws/src/stereo_img_publisher/img/left/000199_10.png'
+        self.right_img_path = '/media/dexter/Xubuntu/catkin_ws/src/stereo_img_publisher/img/right/000199_10.png'
+        
+        self.setup_node()
 
-cv_bridge = CvBridge()
+    
+    def setup_node(self):
+        self.left_img_publisher = rospy.Publisher('stereo_depth_estimation/img/left', Image, queue_size = 1)
+        rospy.init_node('stereo_img_publisher', anonymous = True)
+        rospy.loginfo("Starting stereo_img_publisher.")
 
 
-def publish_image(left_img_publisher):
-    cv_img = cv.imread(left_img_path)
-    ros_img_message = cv_bridge.cv2_to_imgmsg(cv_img, 'bgr8')
-    left_img_publisher.publish(ros_img_message)
-    rospy.loginfo('The image has been published')
+    def publish_image(self):
+        cv_img = cv.imread(self.left_img_path)
+        ros_img_message = self.cv_bridge.cv2_to_imgmsg(cv_img, 'bgr8')
+        self.left_img_publisher.publish(ros_img_message)
+        rospy.loginfo('The image has been published')
 
-
-def setup_node():
-    left_img_publisher = rospy.Publisher('stereo_depth_estimation/img/left', Image, queue_size = 1)
-    str_publisher = rospy.Publisher('chatter', String, queue_size=10)
-    rospy.init_node('stereo_img_publisher', anonymous = True)
-    rospy.loginfo("Starting stereo_img_publisher.")
-
-    return left_img_publisher, str_publisher
 
 if __name__ == "__main__":
-    left_img_publisher, str_publisher = setup_node()  
-    publish_image(left_img_publisher)
-    str_publisher.publish('Elo')
+    ros_node = StereoImagePublisherNode()
+    ros_node.publish_image()
