6965924ffa27ed037b7a5bc668519ee577863561
Dexter <natri4x@gmail.com>
2019-03-12 21:12:24 +0100

    Refactor the main method into  a class

--- stereo_depth_estimation/src/scripts/main.py
+++ stereo_depth_estimation/src/scripts/main.py
@@ -6,69 +6,75 @@ from network import estimate_depth
 from cv_bridge import CvBridge
 import cv2 as cv
 
+class StereoDepthEstimationNode:
+    def __init__(self):
+        rospy.init_node("stereo_depth_estimation", anonymous = True)
+        rospy.loginfo("Starting stereo_depth_estimation")
 
-left_img = None
-left_img_present = False
+        rospy.Subscriber("stereo_depth_estimation/img/left", Image, self.left_img_subscriber)
+        rospy.Subscriber("stereo_depth_estimation/img/right", Image, self.right_img_subscriber)
 
-right_img = None
-right_img_present = False
+        self.left_img_present = False
+        self.right_img_present = False
 
-cv_bridge = CvBridge()
-
-def convert_images_to_cv2():
-    cv_image_left = cv_bridge.imgmsg_to_cv2(left_img, "bgr8")
-    cv_image_right = cv_bridge.imgmsg_to_cv2(right_img, "bgr8")
-
-    return cv_image_left, cv_image_right
-
-
-def run_network_if_both_images_are_present():
-    global left_img_present, right_img_present
-    if left_img_present and right_img_present:
-        cv_image_left, cv_image_right = convert_images_to_cv2()
-
-        rospy.loginfo('Both side images are received, starting depth estimation...')
-        depth = estimate_depth(cv_image_left, cv_image_right)
-        rospy.loginfo('Depth estimation finished!')
-
-        left_img_present = False
-        right_img_present = False
-
-        cv.imshow("out", depth)
-        cv.waitKey(-1)
+        self.left_img = None
+        self.right_img = None
+        
+        self.cv_bridge = CvBridge()
 
 
-def left_img_subscriber(img):
-    global left_img, left_img_present
-    rospy.loginfo('Received left image')
+    def left_img_subscriber(self, img):
+        rospy.loginfo('Received left image')
 
-    if left_img_present == False:
-        left_img = img
-        left_img_present = True
-        run_network_if_both_images_are_present()
+        if self.left_img_present == False:
+
+            self.left_img = img
+            self.left_img_present = True
+
+            if(self.both_images_present):
+                self.run_network()
 
 
 
-def right_img_subscriber(img):
-    global right_img, right_img_present
-    rospy.loginfo('Received right image')
+    def right_img_subscriber(self, img):
+        rospy.loginfo('Received right image')
 
-    if right_img_present == False:
-        right_img = img
-        right_img_present = True
-        run_network_if_both_images_are_present()
+        if self.right_img_present == False:
 
+            self.right_img = img
+            self.right_img_present = True
 
-def string_sub(value):
-    rospy.loginfo(value)
+            if(self.both_images_present):
+                self.run_network()
 
 
-def main():
-    rospy.init_node('stereo_depth_estimation', anonymous = True)
-    rospy.Subscriber("stereo_depth_estimation/img/left", Image, left_img_subscriber)
-    rospy.Subscriber("stereo_depth_estimation/img/right", Image, right_img_subscriber)
+    def both_images_present(self):
+        return self.left_img_present and self.right_img_present
+
+        
+    def run_network(self):
+        if self.left_img_present and self.right_img_present:
+            cv_image_left, cv_image_right = self.convert_images_to_cv2()
+
+            rospy.loginfo('Both side images are received, starting depth estimation...')
+            depth = estimate_depth(cv_image_left, cv_image_right)
+            rospy.loginfo('Depth estimation finished!')
+
+            self.left_img_present = False
+            self.right_img_present = False
+
+            cv.imshow("out", depth)
+            cv.waitKey(-1)
+
+
+    def convert_images_to_cv2(self):
+        cv_image_left = self.cv_bridge.imgmsg_to_cv2(self.left_img, "bgr8")
+        cv_image_right = self.cv_bridge.imgmsg_to_cv2(self.right_img, "bgr8")
+
+        return cv_image_left, cv_image_right
+
+
+
+if __name__ == "__main__":
+    stereo_depth_estimation = StereoDepthEstimationNode()
     rospy.spin()
-
-
-if __name__ == '__main__':
-    main()
